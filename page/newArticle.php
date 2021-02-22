f668cead578ff88b70db64627959410051415705
Fabio <fabio.rdrgs@eduge>
2021-02-22 16:03:20 +0100

    Create newArticle.php

---
+++ page/newArticle.php
@@ -0,0 +1,24 @@
+<?php 
+//Opérateur ternaire
+// VARIABLE = TRUE ? THEN : ELSE;
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link href="" rel="stylesheet" type="text/css"/>  
+    </head>
+    <body>
+        <form method="POST" action="newArticle.php">
+        <label for="tArt">Titre de votre article</label>
+        <input type="text" name="titreArticle" id="tArt"/>
+        <label for="qArt"></label>
+        <input type="number" name="quantiteArticle" id="qArt"/>
+        <label for="dArt"></label>
+        <textarea name="descArticle" id="dArt">
+
+        </textarea>
+        </form>
+    </body>
+</html>
