679692da34bd2dc22ba40d4c68850f1573d328d8
Fabio <fabio.rdrgs@eduge>
2021-03-23 14:38:37 +0100

    DB

---
+++ sql/RefileTesGogosses1_3_0.sql
@@ -0,0 +1,161 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.7
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : mar. 23 mars 2021 à 14:38
+-- Version du serveur :  10.3.27-MariaDB-0+deb10u1
+-- Version de PHP : 7.3.19-1~deb10u1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `RefileTesGogosses`
+--
+CREATE DATABASE IF NOT EXISTS `RefileTesGogosses` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `RefileTesGogosses`;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `t_annonce`
+--
+
+CREATE TABLE `t_annonce` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `nom` varchar(200) NOT NULL,
+  `description` text NOT NULL,
+  `prix` float NOT NULL,
+  `quantite` int(11) NOT NULL,
+  `dateCreation` timestamp NOT NULL DEFAULT current_timestamp(),
+  `idUser` int(10) UNSIGNED DEFAULT NULL,
+  `idCategorie` int(10) UNSIGNED DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `t_categorie`
+--
+
+CREATE TABLE `t_categorie` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `nom` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `t_image`
+--
+
+CREATE TABLE `t_image` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `nomImage` varchar(80) NOT NULL,
+  `typeImage` varchar(50) NOT NULL,
+  `idAnnonce` int(10) UNSIGNED DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `t_user`
+--
+
+CREATE TABLE `t_user` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `nomUtilisateur` varchar(100) NOT NULL,
+  `email` varchar(150) NOT NULL,
+  `mdp` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `t_annonce`
+--
+ALTER TABLE `t_annonce`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idUser` (`idUser`),
+  ADD KEY `idCategorie` (`idCategorie`);
+ALTER TABLE `t_annonce` ADD FULLTEXT KEY `RechercheAnnonces` (`nom`);
+
+--
+-- Index pour la table `t_categorie`
+--
+ALTER TABLE `t_categorie`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `t_image`
+--
+ALTER TABLE `t_image`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idAnnonce` (`idAnnonce`);
+
+--
+-- Index pour la table `t_user`
+--
+ALTER TABLE `t_user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `t_annonce`
+--
+ALTER TABLE `t_annonce`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `t_categorie`
+--
+ALTER TABLE `t_categorie`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `t_image`
+--
+ALTER TABLE `t_image`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `t_user`
+--
+ALTER TABLE `t_user`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `t_annonce`
+--
+ALTER TABLE `t_annonce`
+  ADD CONSTRAINT `idCategorieAnnonce` FOREIGN KEY (`idCategorie`) REFERENCES `t_categorie` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `idUserAnnonce` FOREIGN KEY (`idUser`) REFERENCES `t_user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `t_image`
+--
+ALTER TABLE `t_image`
+  ADD CONSTRAINT `idAnnonceImage` FOREIGN KEY (`idAnnonce`) REFERENCES `t_annonce` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
